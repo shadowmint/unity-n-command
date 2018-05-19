10d3bd444e7408ab4c3d0a56ea1ef89e88d4e1a4
Douglas Linder <douglas.linder@gmail.com>
2018-05-19 12:22:13 +0800

    merge

---
+++ src/n-command/N/Package/Command/CommandExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using N.Package.Bind;
+using N.Package.Bind.Core;
+using N.Packages.Promises;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace N.Package.Command
+{
+  public static class CommandExtensions
+  {
+    public static async Task Execute<T>(this T command, IServiceRegistry registry) where T : ICommand
+    {
+      try
+      {
+        registry = registry ?? Registry.Default;
+        var handler = registry.Resolve<ICommandHandler<T>>();
+        await handler.Execute(command);
+      }
+      catch (Exception error)
+      {
+        throw new CommandExecutionError(command, "Failed to execute task", error);
+      }
+    }
+
+    public static async Task<TResult> Execute<T, TResult>(this T command, IServiceRegistry registry) where T : ICommand<TResult>
+    {
+      try
+      {
+        registry = registry ?? Registry.Default;
+        var handler = registry.Resolve<ICommandHandler<T, TResult>>();
+        return await handler.Execute(command);
+      }
+      catch (Exception error)
+      {
+        throw new CommandExecutionError(command, "Failed to execute task", error);
+      }
+    }
+
+    public static Task Reject<T>(this T commandHandler, CommandExecutionError error) where T : ICommand
+    {
+      var deferred = new Deferred();
+      deferred.Reject(error);
+      return deferred.Task;
+    }
+
+
+    public static Task Reject<T, TResult>(this T commandHandler, CommandExecutionError error) where T : ICommand<TResult>
+    {
+      var deferred = new Deferred();
+      deferred.Reject(error);
+      return deferred.Task;
+    }
+
+    public static Task Resolve<T>(this ICommandHandler<T> commandHandler) where T : ICommand
+    {
+      var deferred = new Deferred();
+      deferred.Resolve(true);
+      return deferred.Task;
+    }
+
+    public static Task<TResult> Resolve<T, TResult>(this ICommandHandler<T, TResult> commandHandler, TResult result) where T : ICommand<TResult>
+    {
+      var deferred = new Deferred<TResult>();
+      deferred.Resolve(result);
+      return deferred.Task;
+    }
+  }
+}
---
+++ src/n-command/N/Package/Command/ICommand.cs
@@ -0,0 +1,16 @@
+﻿namespace N.Package.Command
+{
+  /// <summary>
+  /// The base interface for any command type.
+  /// </summary>
+  public interface ICommand
+  {
+  }
+
+  /// <summary>
+  /// The base interface for any command that returns a value.
+  /// </summary>
+  public interface ICommand<TResult> : ICommand
+  {
+  }
+}
